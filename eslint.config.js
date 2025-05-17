fa242ff39a2b6b77266eeafd7f938d60f3eb3bc2
YuCJ <nakts0123@gmail.com>
2025-05-17 17:51:31 +0800

    ci: add eslint import plugin

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,15 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import importPlugin from "eslint-plugin-import";
 
 export default tseslint.config(
   { ignores: ["dist", "eslint.config.js", ".husky"] },
   js.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
   eslintConfigPrettier,
   {
     files: ["src/**/*.{ts,tsx,js,jsx}"],
@@ -21,16 +24,39 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: ".",
+        },
+
+        alias: {
+          map: [
+            ["@", "./src"],
+            ["", "./public"],
+          ],
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        },
+      },
+    },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "no-console": "error",
+
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
+
+      "import/no-commonjs": "error",
+      "import/first": "error",
+      "import/no-cycle": "error",
+      "import/no-default-export": "error",
     },
   },
   {
