6e8e2b3d17197098e66dd5118e8ae7a5c31b4621
yucj <nakts0123@gmail.com>
2025-05-17 16:29:24 +0800

    chore: add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,14 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default tseslint.config(
   { ignores: ["dist"] },
   js.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  eslintConfigPrettier,
   {
     files: ["src/**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
